3a704f8d1782f7dd915eb94c37b8ce90f282e02d
unknown <kassimalfaheem@gmail.com>
2025-02-03 22:32:01 +0000

    test: added integration tests

--- src/test/java/com/alfacloud/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/alfacloud/catalogservice/CatalogServiceApplicationTests.java
@@ -1,13 +1,41 @@
 package com.alfacloud.catalogservice;
 
+import com.alfacloud.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class CatalogServiceApplicationTests {
+	@Autowired
+	private WebTestClient webTestClient;
+
+	Book expected = new Book("1234567890", "Title", "Author", 9.90);
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void whenPostRequestThenBookCreated() {
+		webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(expected)
+				.exchange()
+				.expectStatus()
+				.isCreated()
+				.expectBody(Book.class)
+				.value(book -> {
+					assertEquals(book.author(), expected.author());
+					assertEquals(book.title(), expected.title());
+					assertEquals(book.price(), expected.price());
+					assertEquals(book.isbn(), expected.isbn());
+				});
+	}
 }
